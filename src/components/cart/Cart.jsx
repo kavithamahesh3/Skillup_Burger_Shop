a051b37f0bcf25b15138045d818feafae7ea9f9c
kavithamahesh3 <kavithacseb03@gmail.com>
2023-12-14 22:05:28 +0100

    Exercise 3 Updates Commit

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import burger1 from "../../assets/burger1.png";
 import burger2 from "../../assets/burger2.png";
 // import burger3 here
-
+import burger3 from "../../assets/burger3.png";
 const CartItem = ({ value, title, img, increment, decrement }) => (
   <div className="cartItem">
     <div>
@@ -32,7 +32,7 @@ const Cart = () => {
           img={burger1}
           value={0}
           increment={() => increment(1)}
-
+          decrement={() => decrement(1)}
         // Add the function for decrementing the order by 1 
        
         />
@@ -42,11 +42,17 @@ const Cart = () => {
           value={0}
           increment={() => increment(2)}
         // Add the function for decrementing the order by 2
-       
+        decrement={() => decrement(2)}
         />
 
         {/* Fill up the code for Cheese Burger similarly */}
-       
+        <CartItem
+          title={"Cheese Burger with French Fries"}
+          img={burger3}
+          value={0}
+          increment={() => increment(3)}
+          decrement={() => decrement(3)}
+        />
 
         <article>
           <div>
