bb2e2276404c7cecd569f79b7391f8aee3b4d072
kavithamahesh3 <kavithacseb03@gmail.com>
2023-12-14 22:52:40 +0100

    Exercise 4 Updates Commit

--- src/components/myOrders/OrderDetails.jsx
+++ src/components/myOrders/OrderDetails.jsx
@@ -1,1 +1,118 @@
 // Write all the code here
+import React from "react";
+
+const OrderDetails = () => {
+  return (
+    <section className="orderDetails">
+      <main>
+        <h1>Order Details</h1>
+        <div>
+          <h1>Shipping</h1>
+          <p>
+            <b>Address</b>
+            {"abcd 23-45sa skahm"}
+          </p>
+        </div>
+        <div>
+          <h1>Contact</h1>
+          <p>
+            <b>Name</b>
+            {"John"}
+          </p>
+          <p>
+            <b>Phone</b>
+            {8456323456}
+          </p>
+        </div>
+
+        <div>
+          <h1>Status</h1>
+          <p>
+            <b>Order Status</b>
+            {"Processing"}
+          </p>
+          <p>
+            <b>Placed At</b>
+            {"10-12-2023"}
+          </p>
+          <p>
+            <b>Delivered At</b>
+            {"10-12-2023"}
+          </p>
+        </div>
+
+        <div>
+          <h1>Payment</h1>
+          <p>
+            <b>Payment Method</b>
+            {"COD"}
+          </p>
+          <p>
+            <b>Payment Reference</b>#{"dfhdfhgjdfgs"}
+          </p>
+          <p>
+            <b>Paid At</b>
+            {"10-12-2023"}
+          </p>
+        </div>
+
+        <div>
+          <h1>Amount</h1>
+          <p>
+            <b>Items Total</b>₹{2132}
+          </p>
+          <p>
+            <b>Shipping Charges</b>₹{200}
+          </p>
+          <p>
+            <b>Tax</b>₹{232}
+          </p>
+          <p>
+            <b>Total Amount</b>₹{232 + 200 + 2132}
+          </p>
+        </div>
+
+        <article>
+          <h1>Ordered Items</h1>
+          <div>
+            <h4>Cheese Burger</h4>
+            <div>
+              <span>{12}</span> x <span>{232}</span>
+            </div>
+          </div>
+          <div>
+            <h4>Veg Cheese Burger</h4>
+            <div>
+              <span>{10}</span> x <span>{500}</span>
+            </div>
+          </div>
+          <div>
+            <h4>Burger Fries</h4>
+            <div>
+              <span>{10}</span> x <span>{1800}</span>
+            </div>
+          </div>
+
+          <div>
+            <h4
+              style={{
+                fontWeight: 700,
+              }}
+            >
+              Sub Total
+            </h4>
+            <div
+              style={{
+                fontWeight: 700,
+              }}
+            >
+              ₹{2132}
+            </div>
+          </div>
+        </article>
+      </main>
+    </section>
+  );
+};
+
+export default OrderDetails;
